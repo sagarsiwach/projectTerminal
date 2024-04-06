e8bae4e0f647ee69debf7bf07d48ec2a5f33bf6d
Sagar Siwach <48592051+sagarsiwach@users.noreply.github.com>
2024-04-06 17:59:36 +0530

    another

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import asyncio
 import httpx
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from resource import increase_production_async, increase_storage_async, start_large_celebration
-# from building import construct_capital
+from building import construct_capital
 from config import read_config
 from login import login
 
@@ -12,15 +11,11 @@ async def main():
 
     # Increase production and storage without a loop
     # await construct_capital(cookies)
-    await increase_storage_async(2500, cookies)
-    await increase_production_async(50000, cookies)
-    # await start_large_celebration(100000, cookies)
-    # print(f"Production completed: {config['production_completed']}, Storage completed: {config['storage_completed']}")
-
-async def scheduled_task():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(main, 'interval', minutes=15)
-    scheduler.start()
+    while True:
+        await increase_storage_async(2500, cookies)
+        await increase_production_async(5000, cookies)
+        # await start_large_celebration(100000, cookies)
+        # print(f"Production completed: {config['production_completed']}, Storage completed: {config['storage_completed']}")
 
 if __name__ == "__main__":
-    asyncio.run(scheduled_task())
+    asyncio.run(main())
