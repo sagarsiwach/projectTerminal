49c29d00673035518f4f34c628e0823c5f25e79d
Sagar Siwach <48592051+sagarsiwach@users.noreply.github.com>
2024-04-06 18:41:11 +0530

    main2

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from resource import increase_production_async, increase_storage_async
 from login import login
 
 async def main():
-    try:
-        cookies = await login()
+    while True:
+        try:
+            cookies = await login()
 
-        # Perform your tasks
-        await increase_storage_async(2500, cookies)
-        await increase_production_async(50000, cookies)
+            # Perform your tasks
+            await increase_storage_async(2500, cookies)
+            await increase_production_async(50000, cookies)
 
-    except Exception as e:
-        print(f"An error occurred: {e}. Restarting script.")
-        subprocess.Popen([sys.executable, 'main.py'])  # Start a new instance of the script
-        sys.exit(1)  # Exit the current script
+        except Exception as e:
+            print(f"An error occurred: {e}. Restarting script.")
+            subprocess.Popen([sys.executable, 'main.py'])  # Start a new instance of the script
+            sys.exit(1)  # Exit the current script
 
 if __name__ == "__main__":
     asyncio.run(main())
